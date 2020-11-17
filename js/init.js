c9ae75704183387799ca01384a2c3d4e42dce392
Sean Doherty <info@seandoherty.co.uk>
2020-11-17 11:48:14 +0000

    first commit

---
+++ js/init.js
@@ -0,0 +1,30 @@
+/*!
+ * Initialisations & customisations
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  /* Init Sidenav (shorthand) */
+
+  // M.Sidenav.init(document.querySelectorAll('.sidenav'));
+
+  /* Init Sidenav (shorthand with options) */
+
+  // M.Sidenav.init(document.querySelectorAll('.sidenav'),{
+  //   edge: 'right'
+  // });
+
+  /* Init Sidenav (as per documentation) */
+
+  // var elems = document.querySelectorAll('.sidenav');
+  // var instances = M.Sidenav.init(elems);
+
+  /* Init Sidenav (as per documentation with options) */
+  
+  // var elems = document.querySelectorAll('.sidenav');
+  // var instances = M.Sidenav.init(elems,{
+  //   edge: 'right'
+  // });
+
+});
+
